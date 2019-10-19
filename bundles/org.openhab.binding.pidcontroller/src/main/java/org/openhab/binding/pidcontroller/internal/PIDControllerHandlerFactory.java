dc1967f7b7c196f62e74484d9c4e55762bd06da2
Dominik Vorreiter <dominikkv@gmx.de>
2019-10-19 22:18:16 +0200

    Refactoring
    Signed-off-by: Dominik Vorreiter <dominikkv@gmx.de>

--- bundles/org.openhab.binding.pidcontroller/src/main/java/org/openhab/binding/pidcontroller/internal/PIDControllerHandlerFactory.java
+++ bundles/org.openhab.binding.pidcontroller/src/main/java/org/openhab/binding/pidcontroller/internal/PIDControllerHandlerFactory.java
@@ -1,13 +1,18 @@
 /**
- * Copyright (c) 2014-2016 by the respective copyright holders.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.pidcontroller.internal;
 
-import static org.openhab.binding.pidcontroller.PIDControllerBindingConstants.THING_TYPE_SAMPLE;
+import static org.openhab.binding.pidcontroller.internal.PIDControllerBindingConstants.THING_TYPE_SAMPLE;
 
 import java.util.Collections;
 import java.util.Set;
@@ -16,7 +21,9 @@ import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
-import org.openhab.binding.pidcontroller.handler.PIDControllerHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.binding.pidcontroller.internal.handler.PIDControllerHandler;
+import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +33,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author George Erhan - Initial contribution
  */
- @Component(service = ThingHandlerFactory.class, configurationPid = "binding.pidcontroller")
+@Component(service = ThingHandlerFactory.class, configurationPid = "binding.pidcontroller")
 public class PIDControllerHandlerFactory extends BaseThingHandlerFactory {
     private Logger logger = LoggerFactory.getLogger(PIDControllerHandlerFactory.class);
     private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
